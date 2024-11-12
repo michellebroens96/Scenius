a30bb7221821c6956f2148cb531c163b317b874c
Anneloes Oude Vrielink <anneloes.oude.vrielink@scenius.nl>
2024-11-12 15:31:35 +0100

    refactor

--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,79 @@
-<template></template>
-<script setup lang="ts"></script>
+<template>
+  <Section>
+    <div class="flex items-center justify-center">
+      <p class="text-gray-400">Enter the amount</p>
+    </div>
+    <span class="flex flex-row items-center justify-center gap-1">
+      <Icon name="material-symbols:euro" class="text-2xl" />
+      <Typography type="heading" size="xxl">{{ input }}</Typography>
+    </span>
+    <div class="flex items-center justify-center">
+      <div class="grid grid-cols-3">
+        <div v-for="button in buttons" :key="button.label">
+          <NumericButton
+            class="text-black"
+            :label="button.label"
+            :icon="button.icon"
+            @click="handleButtonClick(button.label)"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="flex items-center justify-center">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 w-1/4"
+        @click="handlePayment(input)"
+      >
+        <NuxtLink to="/payment">
+          <span class="flex items-center justify-center gap-1">
+            <Icon name="material-symbols:credit-card-outline" />
+            <p>Pay now</p>
+          </span>
+        </NuxtLink>
+      </button>
+    </div>
+  </Section>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Typography from "@/components/partials/Typography.vue";
+import Section from "@/components/layout/Section.vue";
+
+const paymentStore = ref(usePaymentStore());
+const input = ref(paymentStore.value.amountToBePaid);
+
+const props = defineProps({
+  buttons: {
+    type: Array,
+    required: false,
+  },
+});
+
+const buttons = [
+  { label: "1" },
+  { label: "2" },
+  { label: "3" },
+  { label: "4" },
+  { label: "5" },
+  { label: "6" },
+  { label: "7" },
+  { label: "8" },
+  { label: "9" },
+  { label: "", icon: "material-symbols:backspace-outline-rounded" },
+  { label: "0" },
+  { label: "00" },
+];
+
+const handleButtonClick = (label: string) => {
+  if (label === "") {
+    input.value = input.value.toString().slice(0, -1);
+  } else {
+    input.value = input.value.toString() + label;
+  }
+};
+
+const handlePayment = (amount: number) => {
+  paymentStore.value.amountToBePaid = amount;
+};
+</script>
--- pages/payment/index.vue
+++ pages/payment/index.vue
@@ -1,34 +1,39 @@
 <template>
   <Section>
     <div class="flex items-center justify-center">
-      <p class="text-gray-400">Enter the amount</p>
+      <p class="text-gray-400">Present card</p>
     </div>
     <span class="flex flex-row items-center justify-center gap-1">
       <Icon name="material-symbols:euro" class="text-2xl" />
       <Typography type="heading" size="xxl">{{ input }}</Typography>
     </span>
     <div class="flex items-center justify-center">
-      <div class="grid grid-cols-3">
-        <div v-for="button in buttons" :key="button.label">
-          <NumericButton
-            class="text-black"
-            :label="button.label"
-            :icon="button.icon"
-            @click="handleButtonClick(button.label)"
-          />
-        </div>
-      </div>
+      <Icon class="text-[240px]" name="simple-icons:contactlesspayment" />
+    </div>
+    <div class="flex items-center justify-center gap-2">
+      <NuxtLink to="/payment/completed">
+        <button
+          class="hover:bg-gray-300 rounded p-1"
+          @click="setPaymentMethod('Visa')"
+        >
+          <Icon class="w-14 h-14" name="logos:visaelectron" />
+        </button>
+      </NuxtLink>
+      <NuxtLink to="/payment/completed">
+        <button
+          class="hover:bg-gray-300 rounded p-1"
+          @click="setPaymentMethod('MasterCard')"
+        >
+          <Icon class="w-14 h-14" name="logos:mastercard" />
+        </button>
+      </NuxtLink>
     </div>
     <div class="flex items-center justify-center">
       <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 w-1/4"
-        @click="handlePayment(input)"
+        class="bg-gray-800 hover:bg-black text-white font-bold py-2 px-4 rounded mt-4 w-1/4"
       >
-        <NuxtLink to="/payment/transaction">
-          <span class="flex items-center justify-center gap-1">
-            <Icon name="material-symbols:credit-card-outline" />
-            <p>Pay now</p>
-          </span>
+        <NuxtLink to="/payment">
+          <p>Annuleren</p>
         </NuxtLink>
       </button>
     </div>
@@ -36,44 +41,15 @@
 </template>
 
 <script setup lang="ts">
+import { usePaymentStore } from "@/stores/paymentStore"; // Ensure this is correct
 import { ref } from "vue";
-import Typography from "@/components/partials/Typography.vue";
-import Section from "@/components/layout/Section.vue";
 
-const paymentStore = ref(usePaymentStore());
-const input = ref(paymentStore.value.amountToBePaid);
+const paymentStore = usePaymentStore();
+const input = ref(paymentStore.amountToBePaid);
 
-const props = defineProps({
-  buttons: {
-    type: Array,
-    required: false,
-  },
-});
-
-const buttons = [
-  { label: "1" },
-  { label: "2" },
-  { label: "3" },
-  { label: "4" },
-  { label: "5" },
-  { label: "6" },
-  { label: "7" },
-  { label: "8" },
-  { label: "9" },
-  { label: "", icon: "material-symbols:backspace-outline-rounded" },
-  { label: "0" },
-  { label: "00" },
-];
-
-const handleButtonClick = (label: string) => {
-  if (label === "") {
-    input.value = input.value.toString().slice(0, -1);
-  } else {
-    input.value = input.value.toString() + label;
-  }
-};
-
-const handlePayment = (amount: number) => {
-  paymentStore.value.amountToBePaid = amount;
+const setPaymentMethod = (method: string) => {
+  paymentStore.paymentMethod = method;
+  const amount = parseFloat(input.value.replace(",", "."));
+  paymentStore.addPayment(amount, method);
 };
 </script>
